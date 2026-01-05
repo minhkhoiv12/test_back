9dd6ac41f991ac0e8763024f860c6b942aa3c320
minhkhoiv12 <khoitk14033@gmail.com>
2026-01-05 21:09:53 +0700

    dw

---
+++ src/routes/auth.route.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const router = express.Router();
+router.get("/signup", (req,res)=> {
+    res.send("signup endpoint");
+});
+router.get("/login",(req,res)=>{
+    res.send("Login endpoint");
+})
+
+export default router;
