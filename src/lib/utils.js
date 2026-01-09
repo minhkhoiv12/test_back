d092cce5bc0bcd6f8b377210171c167e9759d90e
minhkhoiv12 <khoitk14033@gmail.com>
2026-01-09 15:52:57 +0700

    upda

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,7 +4,7 @@ import { ENV } from "./env.js";
 export const generateToken = (userId, res) => {
   const { JWT_SECRET } = ENV;
   if (!JWT_SECRET) {
-    throw new Error("Chưa cấu hình JWT_SECRET");
+    throw new Error("JWT_SECRET is not configured");
   }
 
   const token = jwt.sign({ userId }, JWT_SECRET, {
@@ -12,12 +12,14 @@ export const generateToken = (userId, res) => {
   });
 
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-    httpOnly: true,
-
-    sameSite: "none", // ✅ cross-domain
-    secure: true,     // ✅ bắt buộc HTTPS
+    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
+    httpOnly: true, // prevent XSS attacks: cross-site scripting
+    sameSite: "none", // CSRF attacks
+    secure: ENV.NODE_ENV === "development" ? false : true,
   });
 
   return token;
 };
+
+// http://localhost
+// https://dsmakmk.com
